58ca0954fab4acac0c4ef91649c3120a53101627
yoann <yoann.chambeux@hotmail.com>
2024-05-15 11:59:52 +0200

    modif3

--- function.js
+++ function.js
@@ -1,7 +1,25 @@
 const functions = require('@google-cloud/functions-framework');
+const express = require('express');
+const app = express();
 
-// Register an HTTP function with the Functions Framework that will be executed
-// when you make an HTTP request to the deployed function's endpoint.
-functions.http('helloGET', (req, res) => {
+app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+app.get('/health', (req, res) => {
+    res.status(204).send();
+});
+
+app.get('/forbidden', (req, res) => {
+    res.status(403).send();
+});
+
+app.use((req, res, next) => {
+    res.status(404).json({ message: 'Not Found' });
+});
+
+functions.http('helloGET', (req, res) => {
+    app(req, res);
+});
+
+module.exports = app;
